b8c619fc289a64d9ddb54708f3b884b07f839bd1
Arshad PA <arshadabupa@gmail.com>
2026-03-12 13:07:38 +0000

    Add generate password function and button

--- 01-password-generator/main.py
+++ 01-password-generator/main.py
@@ -1,4 +1,7 @@
 from tkinter import *
+import random
+import string
+from tkinter import Button
 
 window = Tk()
 window.title("Password Manager")
@@ -8,6 +11,14 @@ Label(text="Website:", bg="white").grid(row=1, column=0)
 entry = Entry(width=35)
 entry.grid(row=1, column=1, columnspan=2)
 entry.focus()
+def generate_password():
+    letters = string.ascii_letters
+    numbers = string.digits
+    symbols = string.punctuation
+    password = ''.join(random.choice(letters + numbers + symbols) for _ in range(12))
+    entry3.delete(0, END)
+    entry3.insert(0, password)
+
 
 # Email
 Label(text="Email/Username:", bg="white").grid(row=2, column=0)
@@ -18,4 +29,7 @@ entry2.grid(row=2, column=1, columnspan=2)
 Label(text="Password:", bg="white").grid(row=3, column=0)
 entry3 = Entry(width=21, show="*")
 entry3.grid(row=3, column=1)
+# Generate Password Button
+generate_button = Button(text="Generate Password", command=generate_password)
+generate_button.grid(row=3, column=2)
 window.mainloop()
